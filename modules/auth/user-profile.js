5c507bef4265410c901d08e976d0a11797ec5d4a
Nit <83890233+nitiphong-nira@users.noreply.github.com>
2025-10-25 17:09:41 +0700

    Create user-profile.js

---
+++ modules/auth/user-profile.js
@@ -0,0 +1,9 @@
+class UserProfile {
+  async getUserInfo(userId) {
+    // ขอข้อมูลโปรไฟล์จาก Line (ชื่อ, รูป)
+  }
+
+  async saveUserProfile(userId, profile) {
+    // เก็บข้อมูล user ใน Google Sheets
+  }
+}
