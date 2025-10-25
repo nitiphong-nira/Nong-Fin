af8d7dcb1d5e8d82d62c47fa2d6cf4d109c234dc
Nit <83890233+nitiphong-nira@users.noreply.github.com>
2025-10-25 23:01:04 +0700

    Update line-api.js

--- modules/utils/line-api.js
+++ modules/utils/line-api.js
@@ -1,5 +1,11 @@
+// modules/utils/line-api.js
 const line = require('@line/bot-sdk');
-const config = require('../../config/line-config');  // ← ใช้ config ไฟล์
+
+const config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET
+};
+
 const client = new line.Client(config);
 
 class LineAPI {
@@ -12,3 +18,5 @@ class LineAPI {
     }
   }
 }
+
+module.exports = LineAPI;
